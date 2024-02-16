78d3fd47c7808ec8b9a436fb9b95e8e95a939851
Sumayer Khan Sajid <139729797+SumayerKhan@users.noreply.github.com>
2024-02-17 03:11:48 +0600

    Create Template.cpp

---
+++ Template.cpp
@@ -0,0 +1,26 @@
+/*******************************************************************
+*  Author : Sumayer Khan Sajid                                     *
+*  University : North South University                             *
+*  Department : Computer Science and Engineering (CSE)             *
+*  Facebook : https://fb.com/sajid002                              *
+*******************************************************************/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl '\n'
+#define PI acos(-1)
+void solve(){
+    
+}
+int main(){
+	optimize();
+    int t; cin >> t;
+    while(t--){
+    	solve();
+    }
+    return 0;
+}
