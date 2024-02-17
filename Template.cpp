c96b548f0bf6d70e4ec74790b65195554ea9a079
Sumayer Khan Sajid <139729797+SumayerKhan@users.noreply.github.com>
2024-02-17 06:02:56 +0600

    Update Template.cpp

--- Template.cpp
+++ Template.cpp
@@ -11,11 +11,17 @@ using namespace std;
 typedef long long ll;
 
 #define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-#define endl '\n'
 #define PI acos(-1)
+#define endl '\n'
+#define pb(x) push_back(x)
+#define mini_pos(a)    ( min_element((a).begin(), (a).end()) - (a).begin())
+#define maxi_pos(a)    ( max_element((a).begin(), (a).end()) - (a).begin())
+
+#define gcd(a,b) __gcd(a,b)
+#define precision(n) fixed<<setprecision(n)
 
 void solve(){
-    
+	
 }
 int main(){
 	optimize();
